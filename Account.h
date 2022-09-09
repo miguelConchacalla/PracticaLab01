5ca9b928bdb735af1f88a04b5ab9a31fa7d88f2d
miguelConchacalla <miguel.conchacalla@ucsp.edu.pe>
2022-09-09 08:24:29 -0500

    semana 3 part2

--- Account.h
+++ Account.h
@@ -1,40 +1,32 @@
-#include <iostream>
 #include <string>
 
-using namespace std;
-
-class Account {
+class Account {    
     public:
-
-    //Constructor
-    Account(string accountName, int initialBalance) : name{accountName}{
-        if (initialBalance > 0){
-            balance = initialBalance;
+        Account(std::string accountName, int initialBalance)
+                : name{accountName} {
+            if(initialBalance > 0) {
+                balance = initialBalance;
+            }
         }
-    }
 
-    void desposit(int depositAmount){
-        if (depositAmount > 0){
-            balance += depositAmount;
+        void deposit(int depositAmount) {
+            if(depositAmount > 0) {
+                balance = balance + depositAmount;
+            }
         }
-    }
-    
-    int getBalance() const {
-        return balance;
-    }
 
-    void setName(string accountName) {
-        name = accountName;
-    }
+        int getBalance() const {
+            return balance;
+        }
 
-    string getName() const {
-        return name;
-    }
+        void setName(std::string accountName) {
+            name = accountName;
+        }
 
+        std::string getName() const {
+            return name;
+        }
     private:
-
-    string name;
-
-    int balance {0};
-
+        std::string name;
+        int balance{0};
 };
